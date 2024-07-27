3ef514afb0636515a3e3f3a1a5ad5debe71c90f9
Guilherme <guirafaelrn@gmail.com>
2024-07-27 18:02:55 -0300

    Adicionar arquivos de projeto.

---
+++ UtmBuilder.Shared/Entities/Utm.cs
@@ -0,0 +1,14 @@
+﻿using UtmBuilder.Shared.ValueObjects;
+
+namespace UtmBuilder.Shared.Entities;
+
+public class Utm
+{
+    public Utm(Url url, Campaign campaign)
+    {
+        Url = url;
+        Campaign = campaign;
+    }
+    public Url Url { get; private set; } 
+    public Campaign Campaign { get; private set; } 
+}
---
+++ UtmBuilder.Shared/ValueObjects/Campaign.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtmBuilder.Shared.ValueObjects;
+
+public class Campaign : ValueObjectBase
+{
+    public Campaign(string source, string medium, string name,
+                    string? id = null, string? term = null, string? content = null)
+    {
+        Source = source;
+        Medium = medium;
+        Name = name;
+        Id = id;
+        Term = term;
+        Content = content;
+    }
+    public string Source { get; private set; }
+    public string Name { get; private set; }
+    public string Medium { get; private set; }
+    public string? Id { get; private set; }
+    public string? Term { get; private set; }
+    public string? Content { get; private set; }
+}
